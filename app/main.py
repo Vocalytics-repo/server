0f126a8b697b8398e09470da9cc5e70e7936e320
NiceGuy1313 <shinjh1013@hanyang.ac.kr>
2025-04-16 14:26:01 +0900

    add:html code for test

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 import socketio
 from event import register_sockets
+from fastapi.responses import HTMLResponse
 
 sio = socketio.AsyncServer(async_mode='asgi')
 fastapi_app = FastAPI()
@@ -14,6 +15,37 @@ fastapi_app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@fastapi_app.get("/", response_class=HTMLResponse)
+def root():
+    return """
+    <html>
+      <head>
+        <title>Vocalytics STT Server</title>
+        <style>
+          body {
+            font-family: Arial, sans-serif;
+            background: #f9f9f9;
+            color: #333;
+            text-align: center;
+            padding-top: 100px;
+          }
+          h1 {
+            font-size: 2.5em;
+            color: #4CAF50;
+          }
+          p {
+            font-size: 1.2em;
+          }
+        </style>
+      </head>
+      <body>
+        <h1>Welcome to Vocalytics's Server</h1>
+        <p>STT Web Service Coming Soon</p>
+        <p>Powered by Nginx + Ubuntu</p>
+      </body>
+    </html>
+    """
 
 register_sockets(sio)
 
