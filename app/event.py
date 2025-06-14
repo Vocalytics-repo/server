cc00f74566b9ba1d744a147a4fb0ad3772f6d1ed
TUAZAR <100247125+TUAZAR@users.noreply.github.com>
2025-06-15 01:08:50 +0900

    refactor: event에 insight 추가

--- app/event.py
+++ app/event.py
@@ -3,6 +3,7 @@ from services.stt.stt import router as stt_router
 from services.tts.google_tts import router as tts_router
 from services.correction.gemini import router as corr_router
 from services.youtube.router import router as youtube_router
+from services.insight.insight_router import router as insight_router
 
 router = APIRouter()
 
@@ -10,3 +11,4 @@ router.include_router(stt_router)
 router.include_router(tts_router)
 router.include_router(corr_router)
 router.include_router(youtube_router)
+router.include_router(insight_router)
